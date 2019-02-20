3b8498db4e333df6fdc8649ca6d8ed998b25bd90
kaufmann-a <kaufmann-a@hotmail.ch>
2019-02-20 22:30:31 +0100

    Added commet with sample ffmpeg command, to implement later

--- musdb18_fileprocessing/musdb18_fileprocessing.py
+++ musdb18_fileprocessing/musdb18_fileprocessing.py
@@ -8,13 +8,17 @@ from os.path import join, isfile, dirname, exists
 import shutil
 
 __author__ = "kaufmann-a@hotmail.ch"
-davidsIP = '//192.168.1.16'
-sources = davidsIP + "/Data/unmix.io/musdb18/splitedToWav"
-destination = davidsIP + "/Data/unmix.io/Trainingsdata/musdb18"
+unmix_server = '//192.168.1.29/unmix-server'
+sources = unmix_server + "/musdb18/splitedToWav"
+destination = unmix_server + "/Trainingdata/musdb18"
 
-def copy_files(sourcedir, outputdir, suffix, type):
+#Beispiel ffmpeg mp4 streams auftrennen und zusammenmergen: ffmpeg -i test.mp4 -filter_complex "[0:1][0:2]amerge=inputs=2[ab]" -map [ab] 1.wav
+#Hier wurden streams 2 und 3 gemerged
+
+def copy_files(sourcedir, outputdir, suffix, type, maxCopy):
     src_files= listdir(sourcedir)
     for file in src_files:
+        if maxCopy <= 0: break
         if file.endswith(suffix):
             new_songname = file[:-suffix.__len__()]
             new_songname_with_type = type + '_' + new_songname + '.wav'
@@ -26,14 +30,17 @@ def copy_files(sourcedir, outputdir, suffix, type):
             shutil.copy(old_file, new_folder)
             if exists(new_songfile): remove(new_songfile)
             rename(copied_songfile, new_songfile)
+            maxCopy -= 1
 
 
 if __name__ == '__main__':
     print('Argument List:', str(sys.argv))
     print('Loading data', file=sys.stderr)
 
-    copy_files(sources, destination, '.stem_audio_track_00.wav', 'mixture')
-    copy_files(sources, destination, '.stem_audio_track_04.wav', 'vocals')
+    maxCopy = 10
+
+    copy_files(sources, destination, '.stem_audio_track_00.wav', 'mixture', maxCopy)
+    copy_files(sources, destination, '.stem_audio_track_04.wav', 'vocals', maxCopy)
 
     print('Finished google files')
 
