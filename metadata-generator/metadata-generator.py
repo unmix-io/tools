ae78ec71ba2e21bf03eee5329d30c4d18146c86c
David Flury <david@flury.email>
2019-02-26 18:45:06 +0100

    Add artist and genre to metadata

--- metadata-generator/metadata-generator.py
+++ metadata-generator/metadata-generator.py
@@ -28,26 +28,36 @@ def generate_metadata(file):
     file_name = file_name.replace(extension, '')
 
     metadata = {}
-    metadata['file_name'] = file_name
+    metadata['file'] = file
+    metadata['path'] = os.path.dirname(file)
+    metadata['extension'] = extension
+    metadata['name'] = file_name
 
-    with audioread.audio_open(file) as f:
-        metadata['channels'] = f.channels
-        metadata['sample_rate'] = f.samplerate
-        metadata['duration'] = f.duration    
-    metadata = spotify_metadata(file_name, metadata)
+    
+    try:
+
+        with audioread.audio_open(file) as f:
+            metadata['channels'] = f.channels
+            metadata['sample_rate'] = f.samplerate
+            metadata['duration'] = f.duration    
+        metadata = spotify_metadata(file_name, metadata)
+    except:
+        pass
     metadata_file = os.path.join(os.path.dirname(file), '%s_metadata.json' % file_name) 
     with open(metadata_file, 'w') as fp:
         json.dump(metadata, fp)
-    print('Generated metafile: %s' % metadata_file)
+        print('Generated metafile: %s' % metadata_file)
 
 
-def spotify_metadata(song_name, metadata):
+def spotify_metadata(song_name, metadata, retry=False):
     response_track = requests.get('https://api.spotify.com/v1/search?q=%s&type=track&limit=1' % song_name, \
         headers={'Authorization': 'Bearer %s' % spotify_token})
     data = json.loads(response_track.text)
     if 'error' in data:
+        if retry:
+            return metadata
         set_spotify_token()
-        return spotify_metadata(song_name, metadata)
+        return spotify_metadata(song_name, metadata, True)
     track = data['tracks']['items'][0]
     metadata['artists'] = []
     for artist in track['artists']:
@@ -59,10 +69,12 @@ def spotify_metadata(song_name, metadata):
     metadata['title'] = track['name']
     response_features = requests.get('https://api.spotify.com/v1/audio-features/%s' % track['id'], \
         headers={'Authorization': 'Bearer %s' % spotify_token})
-    if 'error' in data:
-        set_spotify_token()
-        return spotify_metadata(song_name, metadata)
     features = json.loads(response_features.text)
+    if 'error' in features:
+        if retry:
+            return metadata
+        set_spotify_token()
+        return spotify_metadata(song_name, metadata, True)
     metadata['features'] = {}
     metadata['features']['danceability'] = features['danceability']
     metadata['features']['energy'] = features['danceability']
@@ -73,6 +85,15 @@ def spotify_metadata(song_name, metadata):
     metadata['features']['liveness'] = features['liveness']
     metadata['features']['valence'] = features['valence']
     metadata['features']['tempo'] = features['tempo']
+    response_artist = requests.get('https://api.spotify.com/v1/artists/%s' % track['artists'][0]['id'], \
+        headers={'Authorization': 'Bearer %s' % spotify_token})
+    artist = json.loads(response_features.text)
+    if 'error' in artist:
+        if retry:
+            return metadata
+        set_spotify_token()
+        return spotify_metadata(song_name, metadata, True)
+    metadata['genres'] = artist['genres']
     return metadata
 
 
@@ -87,7 +108,7 @@ def set_spotify_token():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate and store meta data for audio files.')
-    parser.add_argument('--path', default='C:\\temp\\unmix.io\\', type=str, help='Working path')
+    parser.add_argument('--path', default='U:\\2_prepared\\musdb18\\', type=str, help='Working path')
 
     args = parser.parse_args()
     print('Arguments:', str(args))
