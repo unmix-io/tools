48785423a34de16bc3bc22447ed5b99ebe1a2573
kaufmann-a <kaufmann-a@hotmail.ch>
2019-02-28 23:00:38 +0100

    dsd100: added song volume normalization with ratio

--- collection-processing/dsd100_fileprocessing/dsd100_fileprocessing.py
+++ collection-processing/dsd100_fileprocessing/dsd100_fileprocessing.py
@@ -6,14 +6,28 @@ import sys
 from os import listdir, rename, makedirs, remove
 from os.path import join, isfile, dirname, exists
 import shutil
+from pydub import AudioSegment
 import subprocess
 
 __author__ = "kaufmann-a@hotmail.ch"
-unmix_server = '//192.168.1.29/unmix-server'
-sources_dev = unmix_server + "/1_sources/DSD100/Sources/Dev"
-sources_test = unmix_server + "/1_sources/DSD100/Sources/Test"
-sources_out = unmix_server + "/2_prepared/dsd100"
+temp_path = "./temp"
 
+def normalize(file, destination, db = -20.0):
+    def match_target_amplitude(sound, target_dBFS):
+        change_in_dBFS = target_dBFS - sound.dBFS
+        return sound.apply_gain(change_in_dBFS)
+
+    sound = AudioSegment.from_file(file, "wav")
+    normalized_sound = match_target_amplitude(sound, db)
+    normalized_sound.export(destination, format="wav")
+
+def calculate_ratio_instr_vocs(bass, drums_track, restinst_track, vocals_track):
+    level1 = AudioSegment.from_file(bass, "wav").dBFS
+    level2 = AudioSegment.from_file(drums_track, "wav").dBFS
+    level3 = AudioSegment.from_file(restinst_track, "wav").dBFS
+    level4 = AudioSegment.from_file(vocals_track, "wav").dBFS
+    targetDB_VOC = -20 + (-20 * (level4 / ((level1+level2+level3)/3)-1))
+    return targetDB_VOC
 
 def copy_files(sourcedir, outputdir, maxCopy, override):
     src_files = listdir(sourcedir)
@@ -27,6 +41,8 @@ def copy_files(sourcedir, outputdir, maxCopy, override):
         drums_track = join(songfolder, 'drums.wav')
         restinst_track = join(songfolder, 'other.wav')
         vocals_track = join(songfolder, 'vocals.wav')
+        #Calculate ratio instrumentals/vocals and return nromalizationlevel for vocals
+        targetVoc_level = calculate_ratio_instr_vocs(bass_track, drums_track, restinst_track, vocals_track)
 
         new_folder = join(outputdir, folder)
         new_songname_instr = 'instrumental_' + folder + '.wav'
@@ -35,8 +51,9 @@ def copy_files(sourcedir, outputdir, maxCopy, override):
         new_songfile_vocals = join(new_folder, new_songname_vocals)
 
         cmd = "ffmpeg" + ' -i "' + bass_track + '" -i "' + drums_track + '" -i "' + restinst_track + '" -i "' + vocals_track + \
-              "\" -filter_complex \"[0:0][1:0][2:0]amerge=inputs=3[instr]\" -map [instr] -ac 2 \"" + new_songfile_instr + \
-              "\" -map 3:0 -ac 2 \"" + new_songfile_vocals + "\""
+              "\" -filter_complex \"[0:0][1:0][2:0]amix=inputs=3[instr]\" -map [instr] -ac 2 \"" + join(temp_path, new_songname_instr) + \
+              "\" -map 3:0 -ac 2 \"" + join(temp_path, new_songname_vocals) + "\""
+        subprocess.check_call(cmd, shell=True)
 
         if exists(bass_track) and exists(drums_track) and exists(restinst_track) and exists(vocals_track):
             if not exists(new_folder): makedirs(new_folder)
@@ -46,8 +63,11 @@ def copy_files(sourcedir, outputdir, maxCopy, override):
             if exists(new_songfile_vocals) and override:
                 remove(new_songfile_vocals)
             if (not exists(new_songfile_vocals) and not exists(new_songfile_instr)) or override:
-                subprocess.check_call(cmd, shell=True)  # cwd = cwd
+                normalize(join(temp_path, new_songname_instr), new_songfile_instr, -20)
+                normalize(join(temp_path, new_songname_vocals), new_songfile_vocals, targetVoc_level)
                 print("\n" + new_songname_vocals + " and " + new_songname_instr + " converted" + "\n")
+                remove(join(temp_path, new_songname_vocals))
+                remove(join(temp_path, new_songname_instr))
             maxCopy -= 1
 
 
@@ -58,28 +78,17 @@ if __name__ == '__main__':
 
     maxCopy = -1
     override = True
+    unmix_server = '//192.168.1.29/unmix-server'
 
     print('Argument List:', str(sys.argv))
 
-    if sys.argv.__len__() == 1:
-        maxCopy = -1
-        override = True
-    if sys.argv.__len__() >= 2:
-        try:
-            int(sys.argv[1])
-        except ValueError:
-            exit("Add a number as the first argument")
-        if int(sys.argv[1]) >= -1:
-            maxCopy = int(sys.argv[1])
-        else:
-            exit("Enter a number equal or bigger than zero, if you enter -1, the function is disabled")
-    if sys.argv.__len__() >= 3:
-        if sys.argv[2].lower() == "true":
-            override = True
-        elif sys.argv[2].lower() == "false":
-            override = False
-        else:
-            exit("Second argument not valid, enter true or false")
+    if sys.argv.__len__() == 2:
+        unmix_server = sys.argv[1]
+
+    sources_dev = unmix_server + "/1_sources/DSD100/Sources/Dev"
+    sources_test = unmix_server + "/1_sources/DSD100/Sources/Test"
+    sources_out = unmix_server + "/2_prepared/dsd100"
+    if not exists(temp_path): makedirs(temp_path)
 
     copy_files(sources_test, sources_out, maxCopy, override)
     copy_files(sources_dev, sources_out, maxCopy, override)
