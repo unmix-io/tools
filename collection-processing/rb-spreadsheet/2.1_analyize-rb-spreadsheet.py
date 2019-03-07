9e36b6815ed52902451d3cb5c18b9526aa77a457
kaufmann-a <kaufmann-a@hotmail.ch>
2019-03-07 18:59:41 +0100

    rb-spreadsheet progtrammed fileprocessing for rb-spreadsheet folder

--- collection-processing/rb-spreadsheet/2.1_analyize-rb-spreadsheet.py
+++ collection-processing/rb-spreadsheet/2.1_analyize-rb-spreadsheet.py
@@ -1,37 +1,89 @@
 
-from os import listdir, chdir
-from os.path import isfile, join, isdir
+from os import listdir, chdir, makedirs
+from os.path import isfile, join, isdir, exists
+import sys
+import datetime
+import re
+import ntpath
 
 # Format specs
 
 # Format 0: ogg, several tracks, including one named "vocals.ogg"
 c_0_ogg = 0
 
-sourceFolder = "/srv/unmix-server/1_sources/RockBand-GuitarHero/rb-spreadsheet/"
+tempPath = "./temp"
+eligible_simple_filetypes = ['.wav', '.ogg', '.mp3']
 
-sourceFolders = [f for f in listdir(sourceFolder) if isdir(join(sourceFolder, f))]
 
-def handle_fodler(folder):
-    print(folder)
 
-    files = [f for f in listdir(folder) if isfile(join(folder, f))]
+def isVocal(file):
+    return re.search('voc', file, re.IGNORECASE) or re.search('vox', file, re.IGNORECASE)
 
-    if(len([f for f in files if f.__contains__(".ogg")]) > 1):
-        print("ogg found")
-        #c_0_ogg = c_0_ogg + 1
+def file_processing(sourcedir, destdir, maxCopy, override):
+
+    #Create list of directories and songfiles (simple files stands for normal mono or stereo tracks, no containers)
+    directories = [f for f in listdir(sourcedir) if isdir(join(sourcedir, f))]
+    eligible_simple_files = [f for f in listdir(sourcedir) if isfile(join(sourcedir, f)) and eligible_simple_filetypes.__contains__(f[-4:])]
+
+    for d in directories:
+        directoryPath = join(sourcedir, d)
+        file_processing(directoryPath, destdir, maxCopy, override)
+        print(d + "")
+
+    vocals = [f for f in eligible_simple_filetypes if isVocal(f)]
+    instrumentals = [f for f in eligible_simple_filetypes if not isVocal(f)]
+
+    # Abort process if there are no vocals or no instrumentals in folder
+    if (len(vocals) == 0 or len(instrumentals) == 0):
+        return
+
+    # Add new folder in target directory if not yet existing
+    if not exists(join(destdir, ntpath.basename(sourcedir))):
+        makedirs(join(destdir, ntpath.basename(sourcedir)))
+
+
+    #Next: 1. make filter complex, run fulter complex, run normalization with good ratio
     
-    folders = [f for f in listdir(folder) if isdir(join(folder, f))]
-    for fold in folders:
-        handle_fodler(join(folder, fold))
+    # for f in eligible_simple_files:
+    #     file_path = join(sourcedir, f)
+    #     print (file_path)
 
 
 
 
-for f in sourceFolders:
-    folder = join(sourceFolder, f)
-    handle_fodler(folder)
-    #chdir(folder)
-    
+def setup_logfile(log_file_dir):
+    if not exists(log_file_dir):
+        makedirs(log_file_dir)
 
+    log_file_path = join(log_file_dir, datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + "_Logfile.txt")
+    global log_file
+    log_file = open(log_file_path, "w+")
 
-print("Ogg files: " + str(c_0_ogg))
+def init():
+    #Initialize logfile
+    setup_logfile("./logfiles")
+
+    #Create temp folder
+    if not exists(tempPath):
+        makedirs(tempPath)
+
+if __name__ == '__main__':
+    maxCopy = 3
+    override = True
+    unmix_server = "//192.168.1.29/unmix-server"
+
+    print('Argument List:', str(sys.argv))
+
+    if sys.argv.__len__() == 2:
+        unmix_server = sys.argv[1]
+
+    sourcedir = unmix_server + "/1_sources/RockBand-GuitarHero/rb-spreadsheet"
+    destdir = unmix_server + "/2_prepared/RockBand-GuitarHero_rb-spreadsheet"
+
+    init()
+
+    file_processing(sourcedir, destdir, maxCopy, override)
+
+    log_file.close()
+
+    print('Finished converting files')
