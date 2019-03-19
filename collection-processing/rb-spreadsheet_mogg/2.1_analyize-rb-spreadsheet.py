9f81ece10dee87eef7c21687e9c006a43e234db0
kaufmann-a <kaufmann-a@hotmail.ch>
2019-03-20 00:48:19 +0100

    rb-spreadsheet-mogs added new ratio calculation and normalization

--- collection-processing/rb-spreadsheet_mogg/2.1_analyize-rb-spreadsheet.py
+++ collection-processing/rb-spreadsheet_mogg/2.1_analyize-rb-spreadsheet.py
@@ -39,16 +39,6 @@ def has_hidden_attribute(filepath):
     return result
 
 
-def normalize(file, destination, db = -20.0):
-    def match_target_amplitude(sound, target_dBFS):
-        change_in_dBFS = target_dBFS - sound.dBFS
-        return sound.apply_gain(change_in_dBFS)
-
-    sound = AudioSegment.from_file(file, "wav")
-    normalized_sound = match_target_amplitude(sound, db)
-    normalized_sound.export(destination, format="wav")
-
-
 def char_code_for_number(i):
     """ Returns a for 0, b for 1, etc. """
     char = ""
@@ -67,16 +57,10 @@ def mixdown(sourcefolder, destination, tracks_vocs, tracks_instr, songname, pand
     filter_complex_instr_R = ""
     filter_complex_fullmix_L = ""
     filter_complex_fullmix_R = ""
-    sum_volumes_vocs = 0
-    sum_volumes_instr = 0
-    track_count_vocs_L = 0
-    track_count_vocs_R = 0
-    track_count_instr_L = 0
-    track_count_instr_R = 0
-    chain_vocs_L = 0
-    chain_vocs_R = 0
-    chain_instr_L = 0
-    chain_instr_R = 0
+    chain_vocs_L = ""
+    chain_vocs_R = ""
+    chain_instr_L = ""
+    chain_instr_R = ""
 
     track_count = 0
 
@@ -86,10 +70,17 @@ def mixdown(sourcefolder, destination, tracks_vocs, tracks_instr, songname, pand
         with open(join(pandir, songname) + ".csv") as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=';')
             line_count = 0
+            vocals_tracknmbrs = list(map(lambda x: x[:-4], tracks_vocs))
+            instrumentl_tracknmbrs = list(map(lambda x: x[:-4], tracks_instr))
+
             for row in csv_reader:
                 if not line_count == 0:
                     dict_panning[int(row[0])] = row[1]
                 line_count += 1
+            track_count_vocs_L = len([x for x in dict_panning if str(x) in vocals_tracknmbrs and (dict_panning[x] == 'L' or dict_panning[x] == 'M')])
+            track_count_vocs_R = len([x for x in dict_panning if str(x) in vocals_tracknmbrs and (dict_panning[x] == 'R' or dict_panning[x] == 'M')])
+            track_count_instr_L = len([x for x in dict_panning if str(x) in instrumentl_tracknmbrs and (dict_panning[x] == 'L' or dict_panning[x] == 'M')])
+            track_count_instr_R = len([x for x in dict_panning if str(x) in instrumentl_tracknmbrs and (dict_panning[x] == 'R' or dict_panning[x] == 'M')])
     except Exception as inst:
         log_file.write(str(type(inst)) + ": Panningfile of song " + songname + "could not be found")
         log_file.flush()
@@ -99,59 +90,47 @@ def mixdown(sourcefolder, destination, tracks_vocs, tracks_instr, songname, pand
         for tracknr, track in enumerate(tracks_vocs):
             if dict_panning[int(track[:-4])] == "L":
                 char = char_code_for_number(track_count)
-                filter_complex_vocs_L += "[" + str(track_count) + ":0]volume=" + str(1/(len(tracks_vocs) + len(tracks_instr)) / (1/len(tracks_vocs))) + "[" + char + "];"
+                filter_complex_vocs_L += "[" + str(track_count) + ":0]volume=" + str(1/(track_count_vocs_L + track_count_instr_L) / (1/track_count_vocs_L)) + "[" + char + "];"
                 filter_complex_fullmix_L += "[" + str(track_count) + ":0]"
                 chain_vocs_L += "[" + char + "]"
-                track_count_vocs_L += 1
             if dict_panning[int(track[:-4])] == "R":
                 char = char_code_for_number(track_count)
-                filter_complex_vocs_R += "[" + str(track_count) + ":0]volume=" + str(1/(len(tracks_vocs) + len(tracks_instr)) / (1/len(tracks_vocs))) + "[" + char + "];"
+                filter_complex_vocs_R += "[" + str(track_count) + ":0]volume=" + str(1/(track_count_vocs_R + track_count_instr_R) / (1/track_count_vocs_R)) + "[" + char + "];"
                 filter_complex_fullmix_R += "[" + str(track_count) + ":0]"
                 chain_vocs_R += "[" + char + "]"
-                track_count_vocs_R += 1
             if dict_panning[int(track[:-4])] == "M":
                 char_L = char_code_for_number(track_count)
-                filter_complex_vocs_L += "[" + str(track_count) + ":0]volume=" + str(1 / (len(tracks_vocs) + len(tracks_instr)) / (1 / len(tracks_vocs))) + "[" + char_L + "];"
+                filter_complex_vocs_L += "[" + str(track_count) + ":0]volume=" + str(1 / (track_count_vocs_L + track_count_instr_L) / (1 / track_count_vocs_L)) + "[" + char_L + "];"
                 filter_complex_fullmix_L += "[" + str(track_count) + ":0]"
                 chain_vocs_L += "[" + char_L + "]"
-                track_count_vocs_L += 1
                 char_R = char_code_for_number(track_count)
                 filter_complex_vocs_R += "[" + str(track_count) + ":0]"
                 filter_complex_fullmix_R += "[" + str(track_count) + ":0]"
-                filter_complex_vocs_R += "[" + str(track_count) + ":0]volume=" + str(1 / (len(tracks_vocs) + len(tracks_instr)) / (1 / len(tracks_vocs))) + "[" + char_R + "];"
+                filter_complex_vocs_R += "[" + str(track_count) + ":0]volume=" + str(1 / (track_count_vocs_R + track_count_instr_R) / (1 / track_count_vocs_R)) + "[" + char_R + "];"
                 chain_vocs_R += "[" + char_R + "]"
-                track_count_vocs_R += 1
             inp_args += "-i \"" + join(sourcefolder, track )+ "\" "
-            sum_volumes_vocs += AudioSegment.from_file(join(sourcefolder, track), pathlib.Path(track).suffix[1:]).dBFS
             track_count += 1
         for tracknr, track in enumerate(tracks_instr):
             if dict_panning[int(track[:-4])] == "L":
                 char = char_code_for_number(track_count)
-                filter_complex_instr_L += "[" + str(track_count) + ":0]volume=" + str(1/(len(tracks_vocs) + len(tracks_instr)) / (1/len(tracks_instr))) + "[" + char + "];"
+                filter_complex_instr_L += "[" + str(track_count) + ":0]volume=" + str(1/(track_count_vocs_L + track_count_instr_L) / (1/track_count_instr_L)) + "[" + char + "];"
                 filter_complex_fullmix_L += "[" + str(track_count) + ":0]"
                 chain_instr_L += "[" + char + "]"
-                track_count_instr_L += 1
             if dict_panning[int(track[:-4])] == "R":
                 char = char_code_for_number(track_count)
-                filter_complex_instr_R += "[" + str(track_count) + ":0]volume=" + str(1/(len(tracks_vocs) + len(tracks_instr)) / (1/len(tracks_instr))) + "[" + char + "];"
+                filter_complex_instr_R += "[" + str(track_count) + ":0]volume=" + str(1/(track_count_vocs_R + track_count_instr_R) / (1/track_count_instr_R)) + "[" + char + "];"
                 filter_complex_fullmix_R += "[" + str(track_count) + ":0]"
                 chain_instr_R += "[" + char + "]"
-                track_count_instr_R += 1
             if dict_panning[int(track[:-4])] == "M":
                 char_L = char_code_for_number(track_count)
-                filter_complex_instr_L += "[" + str(track_count) + ":0]"
+                filter_complex_instr_L += "[" + str(track_count) + ":0]volume=" + str(1 / (track_count_vocs_L + track_count_instr_L) / (1/track_count_instr_L)) + "[" + char_L + "];"
                 filter_complex_fullmix_L += "[" + str(track_count) + ":0]"
-                filter_complex_instr_L += "[" + str(track_count) + ":0]volume=" + str(1 / (len(tracks_vocs) + len(tracks_instr)) / (1 / len(tracks_instr))) + "[" + char_L + "];"
                 chain_instr_L += "[" + char_L + "]"
-                track_count_instr_L += 1
                 char_R = char_code_for_number(track_count)
-                filter_complex_instr_R += "[" + str(track_count) + ":0]"
+                filter_complex_instr_R += "[" + str(track_count) + ":0]volume=" + str(1 / (track_count_vocs_R + track_count_instr_R) / (1 /track_count_instr_R)) + "[" + char_R + "];"
                 filter_complex_fullmix_R += "[" + str(track_count) + ":0]"
-                filter_complex_instr_R += "[" + str(track_count) + ":0]volume=" + str(1 / (len(tracks_vocs) + len(tracks_instr)) / (1 / len(tracks_instr))) + "[" + char_R + "];"
                 chain_instr_R += "[" + char_R + "]"
-                track_count_instr_R += 1
             inp_args += "-i \"" + join(sourcefolder, track) + "\" "
-            sum_volumes_instr += AudioSegment.from_file(join(sourcefolder, track), pathlib.Path(track).suffix[1:]).dBFS
             track_count += 1
     except Exception as inst:
             log_file.write(str(type(inst)) + ": Pydub could not read " + track)
@@ -160,7 +139,7 @@ def mixdown(sourcefolder, destination, tracks_vocs, tracks_instr, songname, pand
     # Create a fullmix of all audio tracks
     try:
         cmd_fullmix = "ffmpeg " + inp_args + " -filter_complex \"" + filter_complex_fullmix_L + "amix=inputs=" \
-                      + str(track_count) + "[mixL];" + filter_complex_fullmix_R + "amix=inputs=" + str(track_count) \
+                      + str(track_count_vocs_L + track_count_instr_L) + "[mixL];" + filter_complex_fullmix_R + "amix=inputs=" + str(track_count_vocs_R + track_count_instr_R) \
                       + "[mixR];[mixL][mixR]join=inputs=2:channel_layout=stereo[mix]\" -map [mix] -y -ar 44100 \"" \
                       + join(temp_path, "fullmix_" + songname + ".wav") + "\""
         subprocess.check_call(cmd_fullmix, shell=True)
@@ -183,32 +162,20 @@ def mixdown(sourcefolder, destination, tracks_vocs, tracks_instr, songname, pand
               + "amix=inputs=" + str(track_count_instr_L) + ",volume=" + str(differenz_db) + "dB[instrL];" + filter_complex_instr_R + chain_instr_R \
               + "amix=inputs=" + str(track_count_instr_R) + ",volume=" + str(differenz_db) + "dB[instrR];" \
               + "[vocsL][vocsR]join=inputs=2:channel_layout=stereo[vocs];[instrL][instrR]join=inputs=2:channel_layout=stereo[instr]\" -map [vocs] -ac 2 -y -ar 44100 \"" \
-              + join(temp_path, "vocals_" + songname + ".wav") + "\" -map [instr] -ac 2 -y -ar 44100 \"" + join(temp_path, "instrumental_" + songname + ".wav") + "\""
+              + join(destination, "vocals_" + songname + ".wav") + "\" -map [instr] -ac 2 -y -ar 44100 \"" + join(destination, "instrumental_" + songname + ".wav") + "\""
         print(cmd)
         subprocess.check_call(cmd, shell=True)
         log_file.write("Message: " + songname + " was converted successfully\n")
         log_file.flush()
         # For testing purposes
-        # cmd_test = "ffmpeg -i \"" + join(temp_path,
-        #                                  "fullmix_" + songname + ".wav") + "\" -filter_complex \"[0:0]volume=" + str(
-        #     differenz_db) + "dB\" -y -ar 44100 \"" + join(temp_path, "fullmux_norm_" + songname + ".wav") + "\""
-        # subprocess.call(cmd_test, shell=True)
+        cmd_test = "ffmpeg -i \"" + join(temp_path,
+                                         "fullmix_" + songname + ".wav") + "\" -filter_complex \"[0:0]volume=" + str(
+            differenz_db) + "dB\" -y -ar 44100 \"" + join(temp_path, "fullmux_norm_" + songname + ".wav") + "\""
+        subprocess.call(cmd_test, shell=True)
     except Exception as inst:
         log_file.write(str(type(inst)) + ": " + songname + " conversion failed\n")
         log_file.flush()
         return
-
-    # Calculate ratio of volume of mixed vocals and volume of mixed instruments. Instruments will be normalized to
-    # -20 DB, Vocals to -20 DB * Ratio
-    # try:
-    #     target_db_voc = -20 + ( -20 * ((sum_volumes_vocs / tracks_vocs.__len__()) / (sum_volumes_instr / tracks_instr.__len__()) - 1))
-    #     normalize(join(temp_path, "vocals_" + songname) + ".wav", join(destination, "vocals_" + songname) + ".wav", target_db_voc)
-    #     normalize(join(temp_path, "instrumental_" + songname) + ".wav", join(destination, "instrumental_" + songname) + ".wav", -20)
-    #     log_file.write("Message: " + songname + " was normalized successfully\n")
-    #     log_file.flush()
-    # except Exception as inst:
-    #     log_file.write(str(type(inst)) + ": " + songname + " normalization failed\n")
-    #     log_file.flush()
     finally:
         try:
             files = listdir(temp_path)
@@ -259,8 +226,6 @@ def file_processing(sourcedir, destdir, maxCopy, override, pandir):
 
     if not exists(join(destdir, new_foldername)):
         makedirs(join(destdir, new_foldername))
-    elif new_foldername == 'Grateful Dead - Casey Jones.mogg_fixed.mogg_extract':
-        print(new_foldername)
     elif not override:
         return
 
@@ -287,7 +252,7 @@ def init():
 
 if __name__ == '__main__':
     maxCopy = 3
-    override = False
+    override = True
     unmix_server = "//192.168.1.29/unmix-server"
 
     print('Argument List:', str(sys.argv))
