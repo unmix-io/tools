5d541f9b0ef6fb72d37944ac8b8a2c4595cb6181
David Flury <david@flury.email>
2019-04-16 18:37:59 +0200

    Calculate mean and variance over all frequency bins

--- trainingdata-generator/mean-variance-calculator.py
+++ trainingdata-generator/mean-variance-calculator.py
@@ -25,7 +25,7 @@ def calculate_sum(file):
              np.abs(to_complex(data['spectrogram_right'][()]))])
     else:
         channels = np.array([np.abs(to_complex(data['spectrogram'][()]))])
-    return np.sum(channels), np.size(channels)
+    return np.sum(np.sum(channels, axis=2), axis=0), np.size(channels[0])
 
 def calculate_derivation(file, mean):    
     data = h5py.File(file,'r')
@@ -37,7 +37,7 @@ def calculate_derivation(file, mean):
              np.abs(to_complex(data['spectrogram_right'][()]))])
     else:
         channels = np.array([np.abs(to_complex(data['spectrogram'][()]))])
-    return np.sum(np.square(channels - mean))
+    return np.sum(np.sum(np.square(channels - np.repeat(np.reshape(mean, mean.shape + (1,)), channels.shape[-1], axis=1)), axis=2), axis=0)
 
 def to_complex(realimag):
     'Converts the real-imag array of the training values to complex values'
@@ -47,7 +47,7 @@ def to_complex(realimag):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Calculates the mean and variance over h5 audio trainings files.')
-    parser.add_argument('--path', default='D:\\Data\\unmix.io\\4_training\\fft-window=1536_sample-rate=11025_channels=1-mono\\musdb18',
+    parser.add_argument('--path', default='D:\\Data\\unmix.io\\4_training\\fft-window=1536_sample-rate=11025_channels=1-mono\\mini',
          type=str, help='Working path')
 
     args = parser.parse_args()
@@ -56,13 +56,13 @@ if __name__ == '__main__':
     start = time.time()
 
     print('Start calculating mean...')
-    total_sum = 0
+    total_sum = np.zeros((769))
     total_elements = 0
-    total_derivation = 0
+    total_derivation = np.zeros((769))
     files = []
 
     with progressbar.ProgressBar(max_value=progressbar.UnknownLength) as bar:
-        for file in glob.iglob(os.path.join(args.path, '**', '*.h5'), recursive=True):
+        for file in glob.iglob(os.path.join(args.path, '**', 'vocals*.h5'), recursive=True):
             s, e = calculate_sum (file)
             total_sum += s
             total_elements +=  e
@@ -70,7 +70,6 @@ if __name__ == '__main__':
             bar.update(len(files))
         
     total_mean = total_sum / total_elements
-    print('Total mean: %d' % total_mean)
     
     print('Start calculating variance...')
     with progressbar.ProgressBar(max_value=len(files)) as bar:
@@ -82,12 +81,14 @@ if __name__ == '__main__':
     total_variance = total_derivation / (total_elements - 1)
 
     result = {
-        "mean": total_mean,
-        "variance": total_variance,
+        "mean": np.sum(total_mean),
+        "variance": np.sum(total_variance),
+        "bin_mean": total_variance.tolist(),
+        "bin_variance": total_variance.tolist(),
         "files": len(files),
         "path": args.path
     }
-    path = os.path.join(args.path, 'mean-derivation.json')
+    path = os.path.join(args.path, 'vocals_mean-derivation.json')
     with open(path, 'w') as file:
         json.dump(result, file, indent=4)
     
